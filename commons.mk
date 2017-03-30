33d3a8ee8b22648ed8cc1e96dbdac068dd29846d
meilof <meilof@gmail.com>
2017-03-30 11:44:23 +0200

    build fixes
    fix to common cflags, add /usr/local lib/include to make things work on
    mac os

---
+++ commons.mk
@@ -0,0 +1,6 @@
+# cflags for libqap
+CFLAGS_LIB==-std=c++11 -I ../ate-pairing -I ../ate-pairing/include -I /usr/local/include
+
+# cflags/ldflags for tools
+CFLAGS=-std=c++11 -I ../.. -I ../../ate-pairing/include -I /usr/local/include
+LDFLAGS=-L ../../libqap -L ../../ate-pairing/lib -L/usr/local/lib -lqap -lm -lzm -lgmpxx -lgmp
