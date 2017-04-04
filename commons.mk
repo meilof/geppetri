d1d8b8bbf79a29dd9fc08bd72f78daaa5e02dca1
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2017-04-04 12:07:49 +0100

    Remove additional '=' in commons.mk

--- commons.mk
+++ commons.mk
@@ -1,5 +1,5 @@
 # cflags for libqap
-CFLAGS_LIB==-std=c++11 -I ../ate-pairing -I ../ate-pairing/include -I /usr/local/include
+CFLAGS_LIB=-std=c++11 -I ../ate-pairing -I ../ate-pairing/include -I /usr/local/include
 
 # cflags/ldflags for tools
 CFLAGS=-std=c++11 -I ../.. -I ../../ate-pairing/include -I /usr/local/include
